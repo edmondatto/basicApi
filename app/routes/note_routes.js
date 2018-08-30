ecbe4d558bca2b1eabedcd6063d64c370b07ab44
Edmond Atto <edmond091@gmail.com>
2018-08-30 15:19:11 +0300

    Create DELETE, PUT endpoint

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -14,9 +14,9 @@ module.exports = function(app, db) {
   });
 
   app.get('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const details = {'_id': new ObjectID(id) };
-    db.collection('notes').findOne(details, (err, item) => {
+    const idParam = req.params.id;
+    const noteId = {'_id': new ObjectID(idParam) };
+    db.collection('notes').findOne(noteId, (err, item) => {
       if (err) {
         res.send({'error': 'An error has occurred'});
       } else {
@@ -24,4 +24,43 @@ module.exports = function(app, db) {
       }
     })
   });
+
+  app.get('/notes', (req, res) => {
+    db.collection('notes').find({}).toArray((err, items) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(items);
+      }
+    })
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const idParam = req.params.id;
+    const noteId = {'_id': new ObjectID(idParam) };
+
+    db.collection('notes').remove(noteId, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(`Note ${idParam} has been deleted`);
+      }
+    })
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const idParam = req.params.id;
+    const noteId = {'_id': new ObjectID(idParam) };
+    const updatedNote = { title: req.body.title, text: req.body.body };
+
+    db.collection('notes').update(noteId, updatedNote, (err, result) => {
+      if (err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(updatedNote);
+      }
+    })
+
+
+  })
 };
