e6cc7a2203d64c18a8684d2ede4a998947ba7dee
Edmond Atto <edmond091@gmail.com>
2018-08-30 15:00:25 +0300

    Create GET endpoint

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,3 +1,5 @@
+let ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const newNote = { title: req.body.title, text: req.body.body };
@@ -9,5 +11,17 @@ module.exports = function(app, db) {
         res.send(result.ops[0])
       }
     });
+  });
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {'_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    })
   });
 };
